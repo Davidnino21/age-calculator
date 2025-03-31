88e21af471f9976711a4bb4135bb5985c434903a
Davidnino21 <davidnino_21@yahoo.com>
2025-03-31 12:43:39 -0700

    added functions to calculate age and get age

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const button = document.querySelector("button");
 const birthdayEl = document.getElementById("birthday");
 const resultEl = document.getElementById("result");
 
-function getAge() {
-  let today = new Date()
-  let birthDate = new Date("")
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
 }
 
+function getAge(birthdayValue) {
+  const today = new Date();
+  const birthDate = new Date(birthdayValue);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const month = today.getMonth() - birthDate.getMonth();
 
-button.addEventListener("click", function () {
-  console.log(button);
-});
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+button.addEventListener("click", calculateAge);
