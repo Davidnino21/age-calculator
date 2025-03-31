f79d46e845fbcd981f257374320ee8fd572b01c2
Davidnino21 <davidnino_21@yahoo.com>
2025-03-31 12:11:40 -0700

    added button functionality

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function getAge() {
+  let today = new Date()
+  let birthDate = new Date("")
+}
+
+
+button.addEventListener("click", function () {
+  console.log(button);
+});
